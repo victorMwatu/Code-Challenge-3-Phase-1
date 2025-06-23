24c17570efe3fde07b468aff53bf194eb0fc8486
victorMwatu <victormwatu@gmail.com>
2025-06-23 07:12:49 +0300

    added persistence to new posts and handled cancel button, also fixed minor bugs

--- src/index.js
+++ src/index.js
@@ -4,18 +4,30 @@ const addNewPostButton = document.querySelector('.add-post-btn');
 const editPostForm = document.getElementById('editPostForm');
 const editPostButton = document.getElementById('editPostButton');
 const cancelEditPostButton = document.getElementById('cancelEditPostButton');
+const postList = document.getElementById('postList');
+const postCount = document.getElementById('postCount');
+const currentContent = document.getElementById('currentContent');
+const currentImage = document.getElementById('currentImage');
+const currentTitle = document.getElementById('currentTitle');
+const currentMeta = document.getElementById('currentMeta');
+const cancelAddPost = document.getElementById('cancelAddPost');
 //Event listeners 
+addPostForm.addEventListener('submit', addNewPostListener);
 addNewPostButton.addEventListener('click', addPostFormToggle);
 editPostButton.addEventListener('click', editPostFormToggle);
 cancelEditPostButton.addEventListener('click', cancelEditPost);
+postList.addEventListener('click', handlePostClick);
+cancelAddPost.addEventListener('click', handleCancelAddPost);
+//Main function that runs on page load
+document.addEventListener('DOMContentLoaded', main);
+function main() {
+    displayPosts();
+    showPostCount();
+}
+
+ 
 //toggle add new post form when the respective button is clicked
 function addPostFormToggle() {
-    // if(addPostForm.classList.value.includes('active')) {
-    //     addPostForm.classList.remove('active');
-    // }else {
-    //     addPostForm.classList.add('active');
-    // }
-    //Just learnt an easier way below
     addPostForm.classList.toggle('active');
 }
 //show edit post in place of the post when edit button is clicked
@@ -31,4 +43,77 @@ function cancelEditPost() {
     document.getElementById('currentImage').style.removeProperty('display');
     document.getElementById('currentContent').style.removeProperty('display');
     editPostForm.classList.remove('active');
+}
+//Display posts on side panel
+function displayPosts() {
+    
+    fetch('http://localhost:3000/posts')
+    .then(response => response.json())
+    .then( data => data.forEach( post => {
+        const li = document.createElement('li');
+        const titleDiv = document.createElement('div');
+        const metaDiv = document.createElement('div');
+        titleDiv.classList.add('post-title');
+        metaDiv.classList.add('post-meta');
+        li.classList.add('post-item');
+        titleDiv.innerText = post['postTitle'];
+        metaDiv.innerText = `${post['author']} • ${post['date']}`;
+        li.appendChild(titleDiv);
+        li.appendChild(metaDiv);
+        postList.appendChild(li);
+    }
+    ))
+    .catch(error => postList.innerText = `Error: ${error}`);
+}
+function showPostCount() {
+    fetch('http://localhost:3000/posts')
+    .then(response => response.json())
+    .then(d => postCount.innerText = `${d.length} Posts`);
+}
+function handlePostClick(e) {
+    const li = e.target.parentElement;
+    const ol = li.parentElement;
+    const listItems = ol.querySelectorAll('li');
+    listItems.forEach( list => list.classList.remove('active'));//remove active class from other li nodes
+    li.classList.add('active');//set active class for styling, so much work but I put more work on the style 
+    const postName = li.querySelector('div').innerText;
+
+    fetch('http://localhost:3000/posts')
+    .then(response => response.json())
+    .then( data => {
+        const p = data.find( post => post.postTitle === postName);
+        console.log(p);
+        currentContent.innerText = p.post;
+        currentImage.src = p.imageUrl;
+        currentTitle.innerText = p.postTitle;
+        currentMeta.innerText = `${p['author']} • ${p['date']}`;
+    })
+    .catch(error => currentContent.innerText = `Error: ${error}`);
+}
+//Add new post to json.db
+function addNewPostListener(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    data.date = new Date().toISOString().split('T')[0];
+
+    fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(post => {
+        console.log('Post added:', post);
+        addPostForm.reset();
+    })
+    .catch(err => console.error('Error:', err));
+    postList.innerHTML = "";
+    displayPosts();
+}
+function handleCancelAddPost() {
+    addPostForm.reset();
+    addPostFormToggle();
 }
